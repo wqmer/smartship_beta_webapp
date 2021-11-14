16c3ce7199ba302aa0531c64b98e4de71569bd66
Qimin Wang <qiminwang@qimins-imac.lan>
2021-11-14 10:49:33 -0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 // process.env.NODE_ENV = 'development'
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
-}
+// if (process.env.NODE_ENV === "test") {
+//   require("dotenv").config({ path: ".env.test" });
+// } else if (process.env.NODE_ENV === "development") {
+//   require("dotenv").config({ path: ".env.development" });
+// }
 
 module.exports = (env) => {
   const isProduction = env === "production";
