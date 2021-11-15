01bfd7728cb3dfaa94465c4391320e645eca0ece
Qimin Wang <qiminwang@qimins-imac.lan>
2021-11-14 16:58:20 -0800

    update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // host:process.env.HOST || '127.0.0.1',
     port:process.env.PORT || (process.env.NODE_ENV === 'production'?8080:3000),
-    apiUrl:'http://smartship-beta-api-server.herokuapp.com',
+    apiUrl:'http://betasmartshipwebapi-env.eba-trdcmqzd.us-west-2.elasticbeanstalk.com',
     apiHost:process.env.APIHOST || '127.0.0.1',
     apiPort:process.env.APIPORT || '3030',
     dbHost:"localhost",
